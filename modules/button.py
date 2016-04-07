1a865c166250b6a49cf59387fe070e0539a5749a
SchwiftySky <psychociscogaming@gmail.com>
2016-04-06 20:43:56 -0700

    Create button.py

---
+++ modules/button.py
@@ -0,0 +1,49 @@
+from run import battery, frk, car
+
+
+def button():
+    state = True
+    while state == True:
+        print("------------------------------------------")
+        print("Button Solver")
+        print("------------------------------------------")
+        try:
+            buttcolor = str(input("Button color: "))
+            butttext = str(input("Button text: "))
+            if buttcolor == "blue" and butttext == "abort":
+                print("==========================================")
+                print("Hold the button.\nRelease when the timer has a...\nBlue: 4, White: 1, Yellow: 5, Other: 1")
+                print("==========================================")
+                state = False
+            elif battery >= 2 and butttext == "detonate":
+                print("==========================================")
+                print("Press and immediately release the button.")
+                print("==========================================")
+                state = False
+            elif buttcolor == "white" and car == "on":
+                print("==========================================")
+                print("Hold the button.\nRelease when the timer has a...\nBlue: 4, White: 1, Yellow: 5, Other: 1")
+                print("==========================================")
+                state = False
+            elif battery >= 3 and frk == "on":
+                print("==========================================")
+                print("Press and immediately release the button.")
+                print("==========================================")
+                state = False
+            elif buttcolor == "yellow":
+                print("==========================================")
+                print("Hold the button.\nRelease when the timer has a...\nBlue: 4, White: 1, Yellow: 5, Other: 1")
+                print("==========================================")
+                state = False
+            elif buttcolor == "red" and butttext == "hold":
+                print("==========================================")
+                print("Press and immediately release the button.")
+                print("==========================================")
+                state = False
+            else:
+                print("==========================================")
+                print("Hold the button.\nRelease when the timer has a...\nBlue: 4, White: 1, Yellow: 5, Other: 1")
+                print("==========================================")
+                state = False
+        except ValueError:
+            print("You didn't enter a number. Try again.")
