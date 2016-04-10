a5f5aafe67d843e7d99c12a73ace8758b71aa9f2
SchwiftySky <psychociscogaming@gmail.com>
2016-04-09 18:30:17 -0700

    Fixed readability/Added error handling

--- modules/password.py
+++ modules/password.py
@@ -1,232 +1,232 @@
 def password():
-    print("==========================================")
-    print("Password Module Solver")
-    print("==========================================")
-    print("Separate each letter with a comma (eg. a,b,c)")
-    print("------------------------------------------")
-    onesplit = input("Stage one: ")
-    print("------------------------------------------")
-    twosplit = input("Stage two: ")
-    print("------------------------------------------")
-    threesplit = input("Stage three: ")
-    print("------------------------------------------")
-    foursplit = input("Stage four: ")
-    print("------------------------------------------")
-    fivesplit = input("Stage five: ")
+    print("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+")
+    print("|                        PASSWORD                       |")
+    print("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+")
+    print("| Separate each letter with a comma (eg. a,b,c)         |")
+    print("+-------------------------------------------------------+")
+    onesplit = input("| Stage one: ")
+    print("+-------------------------------------------------------+")
+    twosplit = input("| Stage two: ")
+    print("+-------------------------------------------------------+")
+    threesplit = input("| Stage three: ")
+    print("+-------------------------------------------------------+")
+    foursplit = input("| Stage four: ")
+    print("+-------------------------------------------------------+")
     one = onesplit.split(",")
     two = twosplit.split(",")
     three = threesplit.split(",")
     four = foursplit.split(",")
-    five = fivesplit.split(",")
     # About
     if "a" in one and "b" in two:
         if "o" in three and "u" in four:
-            print("==========================================")
-            print("The password is: About")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: About                                |")
+            print("+=======================================================+")
     # After
     elif "a" in one and "f" in two:
         if "t" in three and "e" in four:
-            print("==========================================")
-            print("The password is: After")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: After                                |")
+            print("+=======================================================+")
     # Again
     elif "a" in one and "g" in two:
         if "a" in three and "i" in four:
-            print("==========================================")
-            print("The password is: Again")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Again                                |")
+            print("+=======================================================+")
     # Below
     elif "b" in one and "e" in two:
         if "l" in three and "o" in four:
-            print("==========================================")
-            print("The password is: Below")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Below                                |")
+            print("+=======================================================+")
     # Could
     elif "c" in one and "o" in two:
         if "u" in three and "l" in four:
-            print("==========================================")
-            print("The password is: Could")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Could                                |")
+            print("+=======================================================+")
     # Every
     elif "e" in one and "v" in two:
         if "e" in three and "r" in four:
-            print("==========================================")
-            print("The password is: Every")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Every                                |")
+            print("+=======================================================+")
     # First
     elif "f" in one and "i" in two:
         if "r" in three and "s" in four:
-            print("==========================================")
-            print("The password is: First")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: First                                |")
+            print("+=======================================================+")
     # Found
     elif "f" in one and "o" in two:
         if "u" in three and "n" in four:
-            print("==========================================")
-            print("The password is: Found")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Found                                |")
+            print("+=======================================================+")
     # Great
     elif "g" in one and "r" in two:
         if "e" in three and "a" in four:
-            print("==========================================")
-            print("The password is: Great")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Great                                |")
+            print("+=======================================================+")
     # House
     elif "h" in one and "o" in two:
         if "u" in three and "s" in four:
-            print("==========================================")
-            print("The password is: House")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: House                                |")
+            print("+=======================================================+")
     # Large
     elif "l" in one and "a" in two:
         if "r" in three and "g" in four:
-            print("==========================================")
-            print("The password is: Large")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Large                                |")
+            print("+=======================================================+")
     # Learn
     elif "l" in one and "e" in two:
         if "a" in three and "r" in four:
-            print("==========================================")
-            print("The password is: Learn")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Learn                                |")
+            print("+=======================================================+")
     # Never
     elif "n" in one and "e" in two:
         if "v" in three and "e" in four:
-            print("==========================================")
-            print("The password is: Never")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Never                                |")
+            print("+=======================================================+")
     # Other
     elif "o" in one and "t" in two:
         if "h" in three and "e" in four:
-            print("==========================================")
-            print("The password is: Other")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Other                                |")
+            print("+=======================================================+")
     # Place
     elif "p" in one and "l" in two:
         if "a" in three and "c" in four:
-            print("==========================================")
-            print("The password is: Place")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Place                                |")
+            print("+=======================================================+")
     # Plant
     elif "p" in one and "l" in two:
         if "a" in three and "n" in four:
-            print("==========================================")
-            print("The password is: Plant")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Plant                                |")
+            print("+=======================================================+")
     # Point
     elif "p" in one and "o" in two:
         if "i" in three and "n" in four:
-            print("==========================================")
-            print("The password is: Point")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Point                                |")
+            print("+=======================================================+")
     # Right
     elif "r" in one and "i" in two:
         if "g" in three and "h" in four:
-            print("==========================================")
-            print("The password is: Right")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Right                                |")
+            print("+=======================================================+")
     # Small
     elif "s" in one and "m" in two:
         if "a" in three and "l" in four:
-            print("==========================================")
-            print("The password is: Small")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Small                                |")
+            print("+=======================================================+")
     # Sound
     elif "s" in one and "o" in two:
         if "u" in three and "d" in four:
-            print("==========================================")
-            print("The password is: Sound")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Sound                                |")
+            print("+=======================================================+")
     # Spell
     elif "s" in one and "p" in two:
         if "e" in three and "l" in four:
-            print("==========================================")
+            print("+=======================================================+")
             print("The password is: Spell")
-            print("==========================================")
+            print("+=======================================================+")
     # Still
     elif "s" in one and "t" in two:
         if "i" in three and "l" in four:
-            print("==========================================")
-            print("The password is: Still")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Still                                |")
+            print("+=======================================================+")
     # Study
     elif "s" in one and "t" in two:
         if "u" in three and "d" in four:
-            print("==========================================")
-            print("The password is: Study")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Study                                |")
+            print("+=======================================================+")
     # Their
     elif "t" in one and "h" in two:
         if "e" in three and "i" in four:
-            print("==========================================")
-            print("The password is: Their")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Their                                |")
+            print("+=======================================================+")
     # There
     elif "t" in one and "h" in two:
         if "e" in three and "r" in four:
-            print("==========================================")
+            print("+=======================================================+")
             print("The password is: There")
-            print("==========================================")
+            print("+=======================================================+")
     # These
     elif "t" in one and "h" in two:
         if "e" in three and "s" in four:
-            print("==========================================")
-            print("The password is: These")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: These                                |")
+            print("+=======================================================+")
     # Thing
     elif "t" in one and "h" in two:
         if "i" in three and "n" in four:
-            print("==========================================")
-            print("The password is: Thing")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Thing                                |")
+            print("+=======================================================+")
     # Think
     elif "t" in one and "h" in two:
         if "i" in three and "n" in four:
-            print("==========================================")
-            print("The password is: Think")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Think                                |")
+            print("+=======================================================+")
     # Three
     elif "t" in one and "h" in two:
         if "r" in three and "e" in four:
-            print("==========================================")
-            print("The password is: Three")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Three                                |")
+            print("+=======================================================+")
     # Water
     elif "w" in one and "a" in two:
         if "t" in three and "e" in four:
-            print("==========================================")
-            print("The password is: Water")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Water                                |")
+            print("+=======================================================+")
     # Where
     elif "w" in one and "h" in two:
         if "e" in three and "r" in four:
-            print("==========================================")
-            print("The password is: Where")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Where                                |")
+            print("+=======================================================+")
     # Which
     elif "w" in one and "h" in two:
         if "i" in three and "c" in four:
-            print("==========================================")
-            print("The password is: Which")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Which                                |")
+            print("+=======================================================+")
     # World
     elif "w" in one and "o" in two:
         if "r" in three and "l" in four:
-            print("==========================================")
-            print("The password is: World")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: World                                |")
+            print("+=======================================================+")
     # Would
     elif "w" in one and "o" in two:
         if "u" in three and "l" in four:
-            print("==========================================")
-            print("The password is: Would")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Would                                |")
+            print("+=======================================================+")
     # Write
     elif "w" in one and "r" in two:
         if "i" in three and "t" in four:
-            print("==========================================")
-            print("The password is: Write")
-            print("==========================================")
+            print("+=======================================================+")
+            print("| The password is: Write                                |")
+            print("+=======================================================+")
     else:
-        print("Failed to find a password. Try again.")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("| Failed to find a password. Try again...               |")
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
