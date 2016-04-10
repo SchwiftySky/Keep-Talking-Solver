7a85424b140365bfdd7e1cef907dd8824a6bf84b
SchwiftySky <psychociscogaming@gmail.com>
2016-04-09 19:19:23 -0700

    Fixed readability/Added error handling

--- modules/whos_on_first.py
+++ modules/whos_on_first.py
@@ -1,40 +1,68 @@
 def whos_on_first():
     state = 1
     while state <= 5:
-        tr = "Top Right"
-        mr = "Middle Right"
-        br = "Bottom Right"
-        tl = "Top Left"
-        ml = "Middle Left"
-        bl = "Bottom Left"
-        rdy = "Yes, Okay, What, Middle, Left, Press, Right, Blank, Ready, No, First, Uhhh, Nothing, Wait"
-        frst = "Left, Okay, Yes, Middle, No, Right, Nothing, Uhhh, Wait, Ready, Blank, What, Press, First"
-        no = "Blank, Uhhh, Wait, First, What, Ready, Right, Yes, Nothing, Left, Press, Okay, No, Middle"
-        blnk = "Wait, Right, Okay, Middle, Blank, Press, Ready, Nothing, No, What, Left, Uhhh, Yes, First"
-        nthng = "Uhhh, Right, Okay, Middle, Yes, Blank, No, Press, Left, What, Wait, First, Nothing, Ready"
-        yes = "Okay, Right, Uhhh, Middle, First, What, Press, Ready, Nothing, Yes, Left, Blank, No, Wait"
-        what = "Uhhh, What, Left, Nothing, Ready, Blank, Middle, No, Okay, First, Wait, Yes, Press, Right"
-        uhhh = "Ready, Nothing, Left, What, Okay, Yes, Night, No, Press, Blank, Uhhh, Middle, Wait, First"
-        left = "Right, Left, First, No, Middle, Yes, Blank, What, Uhhh, Wait, Press, Ready, Okay, Nothing"
-        right = "Yes, Nothing, Ready, Press, No, Wait, What, Right, Middle, Left, Uhhh, Blank, Okay, First"
-        middl = "Blank, Ready, Okay, What, Nothing, Press, No, Wait, Left, Middle, Right, First, Uhhh, Yes"
-        ok = "Middle, No, First, Yes, Uhhh, Nothing, Wait, Okay, Left, Ready, Blank, Press, What, Right"
-        wait = "Uhhh, No, Blank, Okay, Yes, Left, First, Press, What, Wait, Nothing, Ready, Right, Middle"
-        press = "Right, Middle, Yes, Ready, Press, Okay, Nothing, Uhhh, Blank, Left, First, What, No, Wait"
-        you = "Sure, You Are, Your, You're, Next, Uh Huh, UR, Hold, What?, You, Uh Uh, Like, Done, U"
-        youar = "Your, next, Like, Uh Huh, What?, Done, Uh Uh, Hold, You, U, You're, Sure, UR, You Are"
-        your = "Uh Uh, You Are, Uh Huh, Your, Next, UR, Sure, U, You're, You, What?, Hold, Like, Done"
-        youre = "You, You're, UR, Next, Uh Uh, You Are, U, Your, What?, Uh Huh, Sure, Done, Like, Hold"
-        ur = "Done, U, UR, Uh Huh, What?, Sure, Your, Hold, You're, Like, Next, Uh Uh, You Are, You"
-        u = "Uh Huh, Sure, Next, What?, You're, UR, Uh Uh, Done, U, You, Like, Hold, You Are, Your"
-        uhhuh = "Uh Huh, Your, You Are, You, Done, Hold, Uh Uh, Next, Sure, Like, You're, UR, U, What?"
-        uhuh = "UR, U, You Are, You're, Next, Uh Uh, Done, You, Uh Huh, Like, Your, Sure, Hold, What?"
-        whatq = "You, Hold, You're, Your, U, Done, Uh Uh, Like, You Are, Uh Huh, UR, Next, What?, Sure"
-        done = "Sure, Uh Huh, Next, What?, Your, UR, You're, Hold, Like, You, U, You Are, Uh Uh, Done"
-        nxt = "What?, Uh Huh, Uh Uh, Your, Hold, Sure, Next, Like, Done, You Are, UR, You're, U, You"
-        hold = "You Are, U, Done, Uh Uh, You, UR, Sure, What?, You're', Next, Hold, Uh Huh, Your, Like"
-        sure = "You Are, Done, Like, You're, You, Hold, Uh Huh, UR, Sure, U, What?, Next, Your, Uh Uh"
-        like = "You're, Next, U, UR, Hold, Done, Uh Uh, What?, Uh HUh, You, Like, Sure, You Are, Your"
+        tr = "| Top Right                                             |"
+        mr = "| Middle Right                                          |"
+        br = "| Bottom Right                                          |"
+        tl = "| Top Left                                              |"
+        ml = "| Middle Left                                           |"
+        bl = "| Bottom Left                                           |"
+        rdy = "| Yes, Okay, What, Middle, Left, Press, Right,          |" \
+              "\n| Blank, Ready, No, First, Uhhh, Nothing, Wait          |"
+        frst = "| Left, Okay, Yes, Middle, No, Right, Nothing,          |" \
+               "\n| Uhhh, Wait, Ready, Blank, What, Press, First          |"
+        no = "| Blank, Uhhh, Wait, First, What, Ready, Right,         |" \
+             "\n| Yes, Nothing, Left, Press, Okay, No, Middle           |"
+        blnk = "| Wait, Right, Okay, Middle, Blank, Press, Ready,       |" \
+            "\n| Nothing, No, What, Left, Uhhh, Yes, First             |"
+        nthng = "| Uhhh, Right, Okay, Middle, Yes, Blank, No,            |" \
+                "\n| Press, Left, What, Wait, First, Nothing, Ready        |"
+        yes = "| Okay, Right, Uhhh, Middle, First, What, Press,        |" \
+            "\n| Ready, Nothing, Yes, Left, Blank, No, Wait            |"
+        what = "| Uhhh, What, Left, Nothing, Ready, Blank, Middle,      |" \
+            "\n| No, Okay, First, Wait, Yes, Press, Right              |"
+        uhhh = "| Ready, Nothing, Left, What, Okay, Yes, Night,         |" \
+            "\n| No, Press, Blank, Uhhh, Middle, Wait, First           |"
+        left = "| Right, Left, First, No, Middle, Yes, Blank,           |" \
+            "\n| What, Uhhh, Wait, Press, Ready, Okay, Nothing         |"
+        right = "| Yes, Nothing, Ready, Press, No, Wait, What,           |" \
+                "\n| Right, Middle, Left, Uhhh, Blank, Okay, First         |"
+        middl = "| Blank, Ready, Okay, What, Nothing, Press, No,         |" \
+                "\n| Wait, Left, Middle, Right, First, Uhhh, Yes           |"
+        ok = "| Middle, No, First, Yes, Uhhh, Nothing, Wait,          |" \
+            "\n| Okay, Left, Ready, Blank, Press, What, Right          |"
+        wait = "| Uhhh, No, Blank, Okay, Yes, Left, First,              |" \
+            "\n| Press, What, Wait, Nothing, Ready, Right, Middle      |"
+        press = "| Right, Middle, Yes, Ready, Press, Okay, Nothing,      |" \
+                "\n| Uhhh, Blank, Left, First, What, No, Wait              |"
+        you = "| Sure, You Are, Your, You're, Next, Uh Huh, UR,        |" \
+            "\n| Hold, What?, You, Uh Uh, Like, Done, U                |"
+        youar = "| Your, next, Like, Uh Huh, What?, Done, Uh Uh,        |" \
+                "\n| Hold, You, U, You're, Sure, UR, You Are              |"
+        your = "| Uh Uh, You Are, Uh Huh, Your, Next, UR, Sure,         |" \
+            "\n| U, You're, You, What?, Hold, Like, Done              |"
+        youre = "| You, You're, UR, Next, Uh Uh, You Are, U,                |" \
+                "\n| Your, What?, Uh Huh, Sure, Done, Like, Hold             |"
+        ur = "| Done, U, UR, Uh Huh, What?, Sure, Your,               |" \
+            "\n| Hold, You're, Like, Next, Uh Uh, You Are, You         |"
+        u = "| Uh Huh, Sure, Next, What?, You're, UR, Uh Uh,         |" \
+            "\n| Done, U, You, Like, Hold, You Are, Your               |"
+        uhhuh = "| Uh Huh, Your, You Are, You, Done, Hold, Uh Uh,        |" \
+                "\n| Next, Sure, Like, You're, UR, U, What?                |"
+        uhuh = "| UR, U, You Are, You're, Next, Uh Uh, Done,            |" \
+            "\n| You, Uh Huh, Like, Your, Sure, Hold, What?            |"
+        whatq = "| You, Hold, You're, Your, U, Done, Uh Uh,              |" \
+                "\n| Like, You Are, Uh Huh, UR, Next, What?, Sure          |"
+        done = "| Sure, Uh Huh, Next, What?, Your, UR, You're,          |" \
+            "\n| Hold, Like, You, U, You Are, Uh Uh, Done              |"
+        nxt = "| What?, Uh Huh, Uh Uh, Your, Hold, Sure, Next,         |" \
+            "\n| Like, Done, You Are, UR, You're, U, You               |"
+        hold = "| You Are, U, Done, Uh Uh, You, UR, Sure,               |" \
+            "\n| What?, You're', Next, Hold, Uh Huh, Your, Like        |"
+        sure = "| You Are, Done, Like, You're, You, Hold, Uh Huh,       |" \
+            "\n| UR, Sure, U, What?, Next, Your, Uh Uh                 |"
+        like = "| You're, Next, U, UR, Hold, Done, Uh Uh,               |" \
+            "\n| What?, Uh HUh, You, Like, Sure, You Are, Your          |"
 
 
         def steptwo(x):
